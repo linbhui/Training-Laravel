7e09e47b07643a859ffd18353d76ccf5ddb7eb13
Linh <181594547+linbhui@users.noreply.github.com>
2025-07-30 11:57:48 +0700

    Routing, Authenticating tests

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -3,31 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\Team;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TeamSeeder extends Seeder
 {
     public function run(): void
     {
-        Team::factory()->create([
-            'name' => 'Team Red',
-            'ins_id' => 1,
-            'del_flag' => 0
-        ]);
+        Team::firstOrCreate(
+            ['name' => 'Team Red'],
+            ['ins_id' => 1, 'del_flag' => 0]
+        );
 
-        Team::factory()->create([
-            'name' => 'Team Blue',
-            'ins_id' => 1,
-            'del_flag' => 0
-        ]);
+        Team::firstOrCreate(
+            ['name' => 'Team Blue'],
+            ['ins_id' => 1, 'del_flag' => 0]
+        );
 
-        Team::factory()->create([
-            'name' => 'Team White',
-            'ins_id' => 1,
-            'del_flag' => 0
-        ]);
-
+        Team::firstOrCreate(
+            ['name' => 'Team White'],
+            ['ins_id' => 1, 'del_flag' => 0]
+        );
     }
 }
-
